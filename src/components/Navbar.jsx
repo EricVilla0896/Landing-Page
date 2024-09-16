bc830747e364c14486b80a7b1927bdafa0aa58c3
rodzpogi <167832369+rodzpogi@users.noreply.github.com>
2024-09-16 20:03:12 +0800

    asdasd

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <nav className="border-b-2"></nav>
+  )
+}
+
+export default Navbar
